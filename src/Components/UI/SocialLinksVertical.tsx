37b4db47726bcc60a8cd4076e17f6c2f7be0c34b
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2023-04-30 13:01:17 -0400

    refactor of social links vertical

--- src/Components/UI/SocialLinksVertical.tsx
+++ src/Components/UI/SocialLinksVertical.tsx
@@ -5,6 +5,27 @@ import { AiOutlineGithub } from "react-icons/ai";
 import { AiTwotoneMail } from "react-icons/ai";
 import { IconContext } from "react-icons";
 
+const SocialLink = ({
+	href,
+	icon,
+	text,
+}: {
+	href: string;
+	icon: JSX.Element;
+	text: string;
+}) => {
+	return (
+		<div style={{ position: "relative" }} className="social-link">
+			<a href={href} target="_blank" rel="noopener noreferrer">
+				{icon}
+				<div className="social-link-text">
+					<span className="social-link-text-span">{text}</span>
+				</div>
+			</a>
+		</div>
+	);
+};
+
 const SocialLinksVertical = () => {
 	return (
 		<IconContext.Provider
@@ -16,58 +37,21 @@ const SocialLinksVertical = () => {
 		>
 			<Wrapper>
 				<div className="social-links-vertical animate__animated animate__fadeInLeft">
-					<div
-						style={{
-							position: "relative",
-						}}
-						className="social-link"
-					>
-						<a
-							href="https://github.com/ZacharyTStone"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<AiOutlineGithub className="social-link-img" />
-							<div className="social-link-text">
-								<span className="social-link-text-span">Github</span>
-							</div>
-						</a>
-					</div>
-					<div
-						style={{
-							position: "relative",
-						}}
-						className="social-link"
-					>
-						<a
-							href="mailto:Zach.Stone.Developer@gmail.com"
-							target="_blank"
-							rel="noopener noreferrer"
-							className="social-link"
-						>
-							<AiTwotoneMail className="social-link-img" />
-							<div className="social-link-text">
-								<span className="social-link-text-span">Email</span>
-							</div>
-						</a>
-					</div>
-					<div
-						style={{
-							position: "relative",
-						}}
-						className="social-link"
-					>
-						<a
-							href="https://www.linkedin.com/in/ZacharyStone42"
-							target="_blank"
-							rel="noopener noreferrer"
-						>
-							<BsLinkedin className="social-link-img" />
-							<div className="social-link-text">
-								<span className="social-link-text-span">LinkedIn</span>
-							</div>
-						</a>
-					</div>
+					<SocialLink
+						href="https://github.com/ZacharyTStone"
+						icon={<AiOutlineGithub className="social-link-img" />}
+						text="Github"
+					/>
+					<SocialLink
+						href="mailto:Zach.Stone.Developer@gmail.com"
+						icon={<AiTwotoneMail className="social-link-img" />}
+						text="Email"
+					/>
+					<SocialLink
+						href="https://www.linkedin.com/in/ZacharyStone42"
+						icon={<BsLinkedin className="social-link-img" />}
+						text="LinkedIn"
+					/>
 				</div>
 			</Wrapper>
 		</IconContext.Provider>
@@ -77,7 +61,7 @@ const SocialLinksVertical = () => {
 const Wrapper = styled.div`
 	.social-links-vertical {
 		position: fixed;
-		left: 0px;
+		left: 0;
 		bottom: 10px;
 		display: flex;
 		justify-content: space-around;
@@ -99,7 +83,7 @@ const Wrapper = styled.div`
 	}
 
 	.social-links-vertical a:hover .social-link-text {
-		color: "var(--secondary-color)" !important;
+		color: var(--secondary-color) !important;
 	}
 
 	.social-link-text {
@@ -110,7 +94,7 @@ const Wrapper = styled.div`
 		width: 75px;
 		height: 20px;
 		background-color: var(--black);
-		border-radius: 0px 0px 20px 20px;
+		border-radius: 0 0 20px 20px;
 		color: var(--secondary-color);
 		font-size: 0.8rem;
 		text-align: center;
@@ -128,7 +112,7 @@ const Wrapper = styled.div`
 		font-size: 1.5rem;
 		text-decoration: none !important;
 		text-align: end;
-		color: "var(--secondary-color)" !important;
+		color: var(--secondary-color) !important;
 		padding-bottom: 15px;
 	}
 
@@ -146,8 +130,6 @@ const Wrapper = styled.div`
 	}
 
 	.social-link-img:hover {
-		-webkit-transform: scale(1.2);
-		-ms-transform: scale(1.2);
 		transform: scale(1.2);
 	}
 `;
