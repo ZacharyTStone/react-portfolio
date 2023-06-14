c7d90a67139b4cd7a70e47fd73fee3b862422b9c
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2023-06-13 22:02:53 -0400

    revert threejs background to old working version

--- src/Components/UI/ThreeJsBackground.js
+++ src/Components/UI/ThreeJsBackground.js
@@ -1,34 +1,20 @@
 import React, { useEffect } from "react";
-import {
-	Scene,
-	PerspectiveCamera,
-	WebGLRenderer,
-	Mesh,
-	PointLight,
-	AmbientLight,
-	TextureLoader,
-	SphereGeometry,
-	MeshStandardMaterial,
-	MathUtils,
-} from "three";
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import mooon from "../../images/threeJS/moon.jpeg";
 import space from "../../images/threeJS/space.jpeg";
-import { debounce } from "lodash";
 
 const ThreeComponent = () => {
-	const moonTexture = new TextureLoader().load(mooon);
-	const spaceTexture = new TextureLoader().load(space);
 	useEffect(() => {
 		// Setup
-		const scene = new Scene();
-		const camera = new PerspectiveCamera(
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(
 			75,
 			window.innerWidth / window.innerHeight,
 			0.1,
 			1000
 		);
-		const renderer = new WebGLRenderer({ antialias: true });
+		const renderer = new THREE.WebGLRenderer({ antialias: true });
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		renderer.setPixelRatio(window.devicePixelRatio);
 		document
@@ -36,33 +22,41 @@ const ThreeComponent = () => {
 			.appendChild(renderer.domElement);
 
 		// Torus
-		const geometry = new SphereGeometry(14, 64, 100);
-		const material = new MeshStandardMaterial({
+		const geometry = new THREE.TorusGeometry(14, 4, 64, 100);
+		const moonTexture = new THREE.TextureLoader().load(mooon);
+		const material = new THREE.MeshStandardMaterial({
 			color: 0x323333,
 			map: moonTexture,
 		});
-		const torus = new Mesh(geometry, material);
+		const torus = new THREE.Mesh(geometry, material);
 		scene.add(torus);
 
 		// Lights
-		const pointLight = new PointLight(0xffffff);
+		const pointLight = new THREE.PointLight(0xffffff);
 		pointLight.position.set(5, 5, 5);
 
-		const ambientLight = new AmbientLight(0xffffff);
+		const ambientLight = new THREE.AmbientLight(0xffffff);
 		scene.add(pointLight, ambientLight);
 
+		// Helpers
+		// const lightHelper = new THREE.PointLightHelper(pointLight)
+		// const gridHelper = new THREE.GridHelper(200, 50);
+		// scene.add(lightHelper, gridHelper)
+
+		// const controls = new OrbitControls(camera, renderer.domElement);
+
 		const addStar = () => {
-			const geometry = new SphereGeometry(0.25, 24, 24);
-			const material = new MeshStandardMaterial({
+			const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+			const material = new THREE.MeshStandardMaterial({
 				color: 0x00ff00,
 				transparent: true,
 				opacity: 0.8,
 			});
-			const star = new Mesh(geometry, material);
+			const star = new THREE.Mesh(geometry, material);
 
 			const [x, y, z] = Array(3)
 				.fill()
-				.map(() => MathUtils.randFloatSpread(150));
+				.map(() => THREE.MathUtils.randFloatSpread(150));
 
 			star.position.set(x, y, z);
 			scene.add(star);
@@ -70,19 +64,26 @@ const ThreeComponent = () => {
 
 		Array(200).fill().forEach(addStar);
 
+		// Background
+
+		const spaceTexture = new THREE.TextureLoader().load(space);
+		// darkens the background space image
+
 		scene.background = spaceTexture;
 
+		// darken background image
+
 		// Scroll Animation
-		const moveCamera = debounce(() => {
+		const moveCamera = () => {
 			const t = document.body.getBoundingClientRect().top;
 
 			// Move the camera based on scroll position
 			camera.position.z = t * -0.09; // Move camera along the z-axis
 			camera.position.x = t * -0.00009; // Move camera along the x-axis
 			camera.rotation.y = t * -0.0002; // Rotate camera around the y-axis
-		}, 10);
+		};
 
-		document.body.addEventListener("scroll", moveCamera);
+		document.body.onscroll = moveCamera;
 		moveCamera();
 
 		// Animation Loop
@@ -93,6 +94,8 @@ const ThreeComponent = () => {
 			torus.rotation.y += 0.0005;
 			torus.rotation.z += 0.001;
 
+			// controls.update();
+
 			renderer.render(scene, camera);
 		};
 
@@ -100,10 +103,10 @@ const ThreeComponent = () => {
 
 		// Clean up
 		return () => {
+			// remove the canvas on unmount
 			document
 				.getElementById("canvas-container")
 				?.removeChild(renderer.domElement);
-			document.body.removeEventListener("scroll", moveCamera);
 		};
 	}, []);
 
