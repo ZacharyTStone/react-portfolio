faece715de775a5fa42554dd52b54a3ba3b663fe
ZacharyTStone <Zach.Stone.Developer@gmail.com>
2023-07-28 19:33:03 -0400

    add down arrow

--- src/Components/Pages/LandingV2/LandingV2.tsx
+++ src/Components/Pages/LandingV2/LandingV2.tsx
@@ -6,9 +6,11 @@ import SocialLinksVertical from "../../UI/SocialLinksVertical";
 import "animate.css";
 import "react-toastify/dist/ReactToastify.css";
 import { createGreeting } from "../../../utils/misc";
-
+import { AiOutlineDown } from "react-icons/ai";
+import { useAppContext } from "../../../context/appContext";
 const LandingV2 = () => {
 	const { t, i18n } = useTranslation();
+	const { showApp } = useAppContext();
 
 	return (
 		<Landing>
@@ -31,6 +33,7 @@ const LandingV2 = () => {
 						<Line>
 							<Word>{t("landing.description1")}</Word>
 						</Line>
+
 						<Line>
 							<Word>{t("landing.description2")}</Word>
 						</Line>
@@ -38,19 +41,62 @@ const LandingV2 = () => {
 				</LandingTile>
 				<SocialLinksVertical />
 			</ContentWrapper>
+			<div
+				className="down-icon-container"
+				onClick={() => {
+					// scroll so #About is into view smoothly
+					document
+						.querySelector("#About")
+						?.scrollIntoView({ behavior: "smooth", block: "start" });
+				}}
+			>
+				<AiOutlineDown
+					size={50}
+					className="down-icon"
+					style={{ color: "var(--off-white)" }}
+				/>
+			</div>
 		</Landing>
 	);
 };
 
 const Landing = styled.div`
+	position: relative;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
-	width: 100%;
+
 	height: 100vh;
 
 	@media (max-width: 768px) {
 		height: calc(100vh - 56px);
+	}
+
+	.down-icon-container {
+		position: absolute;
+		left: calc(50% - 25px);
+		z-index: 9999;
+		bottom: 50px;
+	}
+
+	.down-icon {
+		background: transparent;
+		background-size: cover;
+		border-radius: 50%;
+		transition: transform 0.3s ease-in-out;
+		animation: moveDown 3s infinite;
+	}
+
+	@keyframes moveDown {
+		0% {
+			transform: translateY(0);
+		}
+		50% {
+			transform: translateY(20px); /* Adjust the distance the icon moves */
+		}
+		100% {
+			transform: translateY(0);
+		}
 	}
 `;
 
